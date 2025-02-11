4f2171738ee9273df394366678c48c5ef7c497c3
adibnow-cs <adibnow.cs@gmail.com>
2025-02-11 06:23:38 +0000

    Added New Post section
     UI with Avatar, post input, and media buttons in MiddlePart for adding a new post

--- social_networking/src/pages/components/MiddlePart/MiddlePart.jsx
+++ social_networking/src/pages/components/MiddlePart/MiddlePart.jsx
@@ -1,14 +1,19 @@
-import { Avatar } from "@mui/material";
+import { Avatar, Card, IconButton } from "@mui/material";
 import React from "react";
 import AddIcon from "@mui/icons-material/Add";
 import StoryCircle from "./StoryCircle";
+import ImageIcon from "@mui/icons-material/Image";
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import FeedIcon from '@mui/icons-material/Feed';
 
-
-const story =[11,1,1,1,1]
+const story = [11, 1, 1, 1, 1];
 const MiddlePart = () => {
+  const handleOpenCreatePost = () => {
+    console.log("Open  post model ");
+  };
   return (
     <div className="px-20">
-      <div className="flex items-center p-5 rounded-b-md">
+      <section className="flex items-center p-5 rounded-b-md">
         <div className="flex flex-col items-center mr-4 cursor-pointer">
           <Avatar sx={{ width: "5rem", height: "5rem" }} src=" ">
             <AddIcon sx={{ fontSize: "3rem" }} />
@@ -16,8 +21,40 @@ const MiddlePart = () => {
 
           <p> New</p>
         </div>
-        {story.map((item) => <StoryCircle/>)}
-      </div>
+        {story.map((item) => (
+          <StoryCircle />
+        ))}
+      </section>
+      <Card className="p-5 mt-5">
+        <div className="flex justify-between">
+          <Avatar />
+          <input
+            readOnly
+            className="outline-none w-[90%] rounded-full px-5 bg-transparent border-[#0b23db] border"
+            type="text"
+          />
+        </div>
+        <div className="flex justify-center space-x-9 mt-5">
+          <div className="flex items-center">
+            <IconButton color="primary" onClick={handleOpenCreatePost}>
+              <VideoCallIcon />
+            </IconButton>
+            <span> media</span>
+          </div>
+          <div className="flex items-center">
+            <IconButton color="primary" onClick={handleOpenCreatePost}>
+              <VideoCallIcon />
+            </IconButton>
+            <span> Video</span>
+          </div>
+          <div className="flex items-center">
+            <IconButton color="primary" onClick={handleOpenCreatePost}>
+              <FeedIcon />
+            </IconButton>
+            <span> Article</span>
+          </div>
+        </div>
+      </Card>
     </div>
   );
 };
