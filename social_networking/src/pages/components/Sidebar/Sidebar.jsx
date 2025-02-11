3efeecb98f986bc5f0a97160d6b265565271719b
adibnow-cs <adibnow.cs@gmail.com>
2025-02-11 04:51:41 +0000

    Update  sidebar ui and setup
    Enhanced Sidebar with dynamic navigation, user info, logout menu, and key fixes"

--- social_networking/src/pages/components/Sidebar/Sidebar.jsx
+++ social_networking/src/pages/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { navigationMenu } from "./SidebarNavigation";
-import { Divider } from "@mui/material";
+import { Avatar, Divider } from "@mui/material";
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Fade from '@mui/material/Fade';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const Sidebar = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
   return (
     <div className="card h-sreen flex flex-col justify-between py-5 ">
       <div className="space-y-8 pl-5">
@@ -10,17 +24,47 @@ const Sidebar = () => {
           <span className="logo font-bold text-xl">CineGram</span>
         </div>
         <div className="space-y-8">
-          {navigationMenu.map((item) => (
-            <div className="cursor-pointer flex space-x-3 items-center ">
+          {navigationMenu.map((item,index) => (
+            <div key={index} className="cursor-pointer flex space-x-3 items-center ">
               {item.icon}
               <p className="text-xl"> {item.title} </p>
             </div>
           ))}
         </div>
         <div>
-          <Divider/>
+          <Divider />
+          <div className="pl-5 flex items-center justify-between pt-5">
+            <div className="flex items-center space-x-3 ">
+              <Avatar src="https://st.depositphotos.com/2218212/2938/i/950/depositphotos_29387653-stock-photo-facebook-profile.jpg" />
+              <div>
+                <p className="font-bold">Username</p>{" "}
+                {/*will be replaced using api  */}
+                <p className="opcacity-70"> @username man</p>
+              </div>
+              <Button
+                id="fade-button"
+                aria-controls={open ? "fade-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+              >
+                <MoreVertIcon/>
+              </Button>
+              <Menu
+                id="fade-menu"
+                MenuListProps={{
+                  "aria-labelledby": "fade-button",
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                TransitionComponent={Fade}
+              >
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+              </Menu>
+            </div>
+          </div>
         </div>
-
       </div>
     </div>
   );
