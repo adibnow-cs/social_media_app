2bff6aff146548712fb32e947ff436a72a0bb36a
adibnow-cs <adibnow.cs@gmail.com>
2025-02-15 18:24:03 +0000

    Add more than 1 suggestion
    Suggestion box for various users

--- social_networking/src/pages/components/HomeRight/HomeRight.jsx
+++ social_networking/src/pages/components/HomeRight/HomeRight.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import SearchUser from '../SearchUser/SearchUser'
 import PopularUserCard from './PopularUserCard'
+import { Card } from '@mui/material'
 
 const popularUser=[1,1,1,1,1]
 const HomeRight = () => {
   return (
     <div className='pr-5'>
       <SearchUser/>
-      <div className=' flex justify-between py-5 items-center'>
+      <Card className="p-5">      
+        <div className=' flex justify-between py-5 items-center'>
         <p className='font-semibold opacity-70'> Suggestions for you</p>
         <p className='text-xs font-semibold opacity-95'> View all</p>
-
       </div>
-      <div className='space-y-5'>
-        <PopularUserCard></PopularUserCard>
-
+      <div>
+{popularUser.map((item, index) => <PopularUserCard/>)}
       </div>
+
+      </Card>
+
       
     </div>
   )
