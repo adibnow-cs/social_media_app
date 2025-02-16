ed094528c63b0c54eeb47448215e33486bb15bad
adibnow-cs <adibnow.cs@gmail.com>
2025-02-16 01:39:21 +0000

    Fixing suggestion box issue in profile page
    Suggestion box was coming on the left hand side in the profile page

--- social_networking/src/pages/Homepage/Homepage.jsx
+++ social_networking/src/pages/Homepage/Homepage.jsx
@@ -13,39 +13,34 @@ function Homepage() {
   return (
     <div className="px-20">
       <Grid container spacing={2} justifyContent="space-between">
-         
-         {/* Sidebar (Left Section) */}
+        {/* Sidebar (Left Section) */}
 
         <Grid item xs={0} lg={3}>
           <div className="sticky top-0">
             <Sidebar />
           </div>
         </Grid>
-        
+
         {/* Middle Content (Center Section) */}
-                
-        <Grid
-          item
-          className="px-5 flex justify-center "
-          xs={12}
-          lg={6}
-        >
+
+        <Grid item className="px-5 flex justify-center " xs={12} lg={6}>
           <Routes>
-            <Route path ="/" element ={<MiddlePart/>} />
-            <Route path ="/reels" element ={<Reels/>} />
-            <Route path ="/create-reels" element ={<CreateReels/>} />
-            <Route path ="/profile/:id" element ={<ProfilePage/>} />
+            <Route path="/" element={<MiddlePart />} />
+            <Route path="/reels" element={<Reels />} />
+            <Route path="/create-reels" element={<CreateReels />} />
+            <Route path="/profile/:id" element={<ProfilePage />} />
           </Routes>
         </Grid>
-        
-        {/* HomeRight (Right Section) */}
-        
-        <Grid item lg={3} className="relative">
-          <div className="sticky top-0 w-full">
-            <HomeRight/>
-          </div>
 
-        </Grid>
+        {/* HomeRight (Right Section) */}
+
+        {location.pathname === "/" && (
+          <Grid item lg={3} className="relative">
+            <div className="sticky top-0 w-full">
+              <HomeRight />
+            </div>
+          </Grid>
+        )}
       </Grid>
     </div>
   );
